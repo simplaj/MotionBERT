53dfcac41b62a20c8781d59f86498e8ac8c45977
tzh <zhihuatean@gmail.com>
2024-05-08 09:02:58 +0000

    add test

---
+++ ft.sh
@@ -0,0 +1,5 @@
+python train_action.py \
+--config configs/action/MB_ft_NTU60_xsub.yaml \
+--pretrained checkpoint/pretrain/ \
+--checkpoint checkpoint/action/FT_MB_release_MB_ft_NTU60_xsub \
+--selection best_epoch.bin
---
+++ test.sh
@@ -0,0 +1,3 @@
+python train_action.py \
+--config configs/action/MB_ft_NTU60_xsub.yaml \
+--evaluate checkpoint/action/FT_MB_release_MB_ft_NTU60_xsub/latest_epoch.bin 
